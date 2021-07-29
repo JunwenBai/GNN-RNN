Minor (fix divide by 0 in MAPE computation, only compute Y mean/std on train years

cnn-rnn/train.py
@@ -63,7 +63,7 @@ def eval(pred, Y, args):
         # MSE
         metrics['mse'][output_name] = np.mean((pred_i-Y_i)**2)
         # MAPE
-        metrics['mape'][output_name] = np.mean(np.abs((Y_i - pred_i) / Y_i))
+        metrics['mape'][output_name] = np.mean(np.abs((Y_i - pred_i) / (Y_i + 1e-5)))
 
     # For each metric, average over all outputs
     for metric_name in metrics:

cnn-rnn/utils.py
@@ -172,15 +172,11 @@ def get_X_Y(data, args, device):
 
 
     # Compute average of each output
-    means = []
-    stds = []
-    for i in range(Y_train.shape[2]):
-        Y_i = Y_train[:, -1, i]
-        Y_i = Y_i[~np.isnan(Y_i)]
-        means.append(np.mean(Y_i))
-        stds.append(np.std(Y_i))
-    args.means = torch.tensor(means, device=device)
-    args.stds = torch.tensor(stds, device=device)
+    Y_mean = np.nanmean(Y[known_years], axis=0, keepdims=True)
+    Y_std = np.nanstd(Y[known_years], axis=0, keepdims=True)
+    args.means = torch.tensor(Y_mean, device=device)
+    args.stds = torch.tensor(Y_std, device=device)
+    print("Y (output) means", args.means, "stds", args.stds)
 
     # Create dictionaries mapping from (county + year) to features/labels
     X_dict = {}